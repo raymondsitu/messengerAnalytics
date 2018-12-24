4da01020e6f89d4f9156ff197a4abf17114e61c2
raymondsitu <situ.raymond@hotmail.com>
2018-12-23 22:04:26 -0800

    Added new features and refactored
    Added avg number of pictures
    Better message polarity trend plots with date range
    Most positive and most negative messages

--- messengerAnalytics.py
+++ messengerAnalytics.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 import json
 import datetime
-import os
+import os, errno
 import pickle
 import math
 import random
 import numpy as np
 import pandas as pd
+import re
 
 import matplotlib
 matplotlib.use('Agg')
@@ -16,48 +16,78 @@ import seaborn as sns
 from sklearn.preprocessing import scale
 from textblob import TextBlob
 
+MY_NAME = 'Raymond Situ'
 DIR = "./data/messages/"
 MIN_CONVO_LENGTH = 7000
+POLARITY_BIN_SIZE = 200
+K_POLARITY_MESSAGES = 10
+METRIC_LIST = ['avg_msg', 'sent_receive_ratio', 'responsiveness', 'sent_receive_word_ratio', 'overall', 'avg_polarity', 'avg_photo_share']
 stopwords = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don\'t', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than', 'lol', 'yeah', 'u', 'ok', 'okay', 'yea', 'im', 'like', 'get', 'it\'s', 'ohh', 'got', 'ur', 'i\'m', 'oh', 'good', 'hey', 'sure', 'that\'s', 'ya', 'haha', 'also', 'you\'re', 'he\'s', 'dont', 'didn\'t','one', 'well', 'still','use',':)',':(','yup','uh','i\'ll', 'much', 'really', 'tho', 'kk', 'yes', 'man', 'yo'}
 
 dataset = dict()
 metrics = dict()
-metrics['avg_msg'] = dict()
-metrics['sent_receive_ratio'] = dict()
-metrics['responsiveness'] = dict()
-metrics['sent_receive_word_ratio'] = dict()
-metrics['overall'] = dict()
-metrics['avg_polarity'] = dict()
+
+for m in METRIC_LIST:
+    metrics[m] = dict()
 
 polarity = dict()
+polarity_global_df = pd.DataFrame.from_dict({'conversation':[],'sender':[],'message':[], 'polarity':[]})
+photo_count = dict()
+
+# helper to get name of other conversation participant
+def get_name(participants):
+    names = [person['name'] for person in participants]
+    names.remove(MY_NAME)
+    return str(names[0])
 
 # read data
 def read_json(foldername):
-    try:
-        with open('{}{}/message.json'.format(DIR, foldername)) as infile:
+    message_dir = '{}{}/message.json'.format(DIR, foldername)
+    if not os.path.exists(message_dir):
+        return ('',[])
+    with open(message_dir) as infile:
+        data = json.load(infile)
+        if len(data['participants']) == 2 and len(data['messages']) >= MIN_CONVO_LENGTH:
+            name = get_name(data['participants'])
+            return (name,data['messages'])
+        else:
+            return ('',[])
+
+# count number of photos shared in a conversation
+def count_pictures():
+    global photo_count
+    for foldername in os.listdir(DIR):
+        message_dir = '{}{}/message.json'.format(DIR, foldername)
+        if not os.path.exists(message_dir):
+            continue
+        with open(message_dir) as infile:
             data = json.load(infile)
-            if len(data['participants']) == 2 and len(data['messages']) > MIN_CONVO_LENGTH:
-                return data['messages']
-            else:
-                return []
-    except:
-        return []
+            if len(data['participants']) != 2 or len(data['messages']) < MIN_CONVO_LENGTH:
+                continue
+            name = get_name(data['participants'])
+            photo_directory = '{}{}/photos'.format(DIR, foldername)
+            if os.path.exists(photo_directory):
+                photo_count[name] = len(os.listdir(photo_directory))
 
 # read data and load into a dict
 def load_dict():
+    print 'loading data'
     global dataset
     if os.path.exists('./data.pickle'):
         with open('./data.pickle', 'rb') as infile:
             dataset = pickle.load(infile)
     else:
         for foldername in os.listdir(DIR):
-            json_data = read_json(foldername)
+            name,json_data = read_json(foldername)
             if len(json_data) > 0:
-                print foldername.split('_')[0], len(json_data)
-                dataset[(foldername.split('_')[0]).lower()] = json_data
+                print name, len(json_data)
+                dataset[name] = json_data
+        pickle_dataset()
+    count_pictures()
 
 # cache the dict
-def pickel_dataset():
+def pickle_dataset():
+    print 'pickling data'
     with open('data.pickle', 'wb') as outfile:
         pickle.dump(dataset, outfile, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -66,18 +96,25 @@ def get_avg_msg(messages):
     last_ts = datetime.datetime.fromtimestamp(messages[0]['timestamp_ms']/1000.0)
     first_ts = datetime.datetime.fromtimestamp(messages[-1]['timestamp_ms']/1000.0)
     days = (last_ts - first_ts).days
-    return round(float(len(messages))/days,2)
+    return float(len(messages))/days
+
+# Avg number of photos shared over active friendship period
+def get_avg_photos(messages, name):
+    last_ts = datetime.datetime.fromtimestamp(messages[0]['timestamp_ms']/1000.0)
+    first_ts = datetime.datetime.fromtimestamp(messages[-1]['timestamp_ms']/1000.0)
+    days = (last_ts - first_ts).days
+    return float(photo_count[name])/days
 
 # Ratio of messages sent by me to messages received
 def get_sent_receive_ratio(messages):
     sent = 0
     receive = 0
     for msg in messages:
-        if msg['sender_name'] == 'Raymond Situ':
+        if msg['sender_name'] == MY_NAME:
             sent +=1
         else:
             receive +=1
-    return round(float(sent)/receive,2)
+    return float(sent)/receive
 
 # Ratio of words in messages sent by me to words in messages received
 def get_sent_receive_word_ratio(messages):
@@ -90,17 +127,42 @@ def get_sent_receive_word_ratio(messages):
         number_of_words = len(filtered_words)
         if number_of_words == 0:
             continue
-        if msg['sender_name'] == 'Raymond Situ':
+        if msg['sender_name'] == MY_NAME:
             sent_words += number_of_words
         else:
             receive_words += number_of_words
-    return round(float(sent_words)/receive_words,2)
+    return float(sent_words)/receive_words
+
+# get the avg polarity of messages
+def get_avg_polarity(messages, name):
+    result = []
+    df_dict = dict()
+    df_dict['conversation'] = []
+    df_dict['sender'] = []
+    df_dict['message'] = []
+    df_dict['polarity'] = []
+    df_dict['msg_length'] = []
+    for msg in reversed(messages):
+        if 'content' not in msg.keys():
+            continue
+        message_content = msg['content']
+        message_tb = TextBlob(message_content)
+        polarity = message_tb.sentiment.polarity
+        result.append(polarity)
+        if len(message_tb.words) > 5:
+            df_dict['conversation'].append(name)
+            df_dict['sender'].append(msg['sender_name'])
+            df_dict['message'].append(filter_characters(message_content))
+            df_dict['polarity'].append(polarity)
+            df_dict['msg_length'].append(len(message_content))
+    add_to_global_polarity(df_dict)
+    return np.mean(result)
 
 # Responsiveness (higher number means quicker to respond)
 def get_responsiveness(messages):
     ts_list = []
     diff_list = []
-    last_msg_from = 'Raymond Situ'
+    last_msg_from = MY_NAME
     for msg in messages:
         if msg['sender_name'] != last_msg_from:
             ts_list.append(msg['timestamp_ms'])
@@ -112,42 +174,73 @@ def get_responsiveness(messages):
 
 # overall score
 def get_overall_friendship_score(name):
-    avg_msg = metrics['avg_msg'][name]
+    max_msg = max(metrics['avg_msg'].values())
+    max_photo = max(metrics['avg_photo_share'].values())
+    max_responsiveness = max(metrics['responsiveness'].values())
+    avg_msg = metrics['avg_msg'][name]/max_msg
+    avg_photo_share = metrics['avg_photo_share'][name]/max_photo
     sent_receive_ratio = metrics['sent_receive_ratio'][name]
     sent_receive_word_ratio = metrics['sent_receive_word_ratio'][name]
-    responsiveness = metrics['responsiveness'][name]
-    return (avg_msg*responsiveness)/((sent_receive_ratio*sent_receive_word_ratio)**0.5)
+    responsiveness = metrics['responsiveness'][name]/max_responsiveness
+    return ((avg_msg + avg_photo_share*0.2)*responsiveness)/((sent_receive_ratio*sent_receive_word_ratio)**0.5)
 
-# get the avg polarity of messages
-def get_avg_polarity(messages, name):
-    result = list()
+# adds top k negative and positive polarity messages for each sender to the global df
+def add_to_global_polarity(df_dict):
+    global polarity_global_df
+    df = pd.DataFrame.from_dict(df_dict)
+    senders = df['sender'].unique()
+    for sender in senders:
+        polarity_global_df = polarity_global_df.append(
+            df[df['sender'] == sender].sort_values(by=['polarity', 'msg_length'], ascending=False)
+            .drop(columns=['msg_length'])
+            .head(K_POLARITY_MESSAGES), sort=True)
+        polarity_global_df = polarity_global_df.append(
+            df[df['sender'] == sender].sort_values(by=['polarity', 'msg_length'], ascending=True)
+            .drop(columns=['msg_length'])
+            .head(K_POLARITY_MESSAGES), sort=True)
+
+# export df of top negative and positive polarity messages to csv
+def save_global_polarity_df():
+    polarity_global_df[['conversation','sender','polarity','message']].to_csv('max_min_polarity_messages.csv',index=False,encoding='utf-8')
+
+# group the messages into bins and take polarity of each bin
+def plot_bin_polarity(messages, name):
+    global polarity
+    ts_list = []
+    msg_list = []
     for msg in reversed(messages):
         if 'content' not in msg.keys():
             continue
-        message_text = TextBlob(msg['content'])
-        polarity = message_text.sentiment.polarity
-        result.append(polarity)
-    bin_polarity(result, name)
-    return round(np.mean(result),2)
-
-# split the polarity of messages into bins and take avg of each bin
-def bin_polarity(polarity_lst, name):
-    global polarity
-    bins = np.array_split(polarity_lst, 200)
+        ts_list.append(msg['timestamp_ms'])
+        msg_list.append(msg['content'])
+    ts_bin = np.array_split(ts_list, POLARITY_BIN_SIZE)
+    msg_bins = np.array_split(msg_list, POLARITY_BIN_SIZE)
+    list_of_date = []
     list_of_avgs = []
-    for b in bins:
-        list_of_avgs.append(np.mean(b))
+    for txt in msg_bins:
+        cleaned_txt = [t.rstrip('.') for t in txt]
+        message_tb = TextBlob('.'.join(cleaned_txt))
+        list_of_avgs.append(message_tb.sentiment.polarity)
+    for ts in ts_bin:
+        midpoint = (ts[-1] + ts[0])/2
+        list_of_date.append(datetime.datetime.fromtimestamp(midpoint/1000.0).date())
     polarity[name] = list_of_avgs
-    plot_polarity_trend(list_of_avgs, name.lower())
+    plot_polarity_trend_helper((list_of_date, list_of_avgs), name)
 
-# plot individual polarity trend for an individual
-def plot_polarity_trend(list_of_avgs, name):
+# Helper function to plot polarity trend for an individual with time
+def plot_polarity_trend_helper(datapoints, name):
     plt.figure(figsize=(15,5))
-    plt.plot(range(200), list_of_avgs, label=name)
+    plt.plot(range(len(datapoints[1])), datapoints[1], label=name)
     plt.title('message polarity trend {}'.format(name))
-    plt.tick_params(axis='x',labelbottom=False)
-    plt.yticks(np.arange(min(list_of_avgs), max(list_of_avgs)+0.1, 0.025))
+    plt.xticks(range(0,210,10),datapoints[0][::9][:21],rotation=90)
+    plt.yticks(np.arange(-0.6,0.7,0.05))
     plt.tight_layout()
+    plt.ylabel('polarity [-1,1]')
+    try:
+        os.makedirs('./polarity_trends')
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     plt.savefig('./polarity_trends/polarity_trend_{}.png'.format(name), dpi=200, bbox_inches='tight')
     plt.cla()
     plt.clf()
@@ -157,15 +250,24 @@ def plot_polarity_trend(list_of_avgs, name):
 def plot_polarity_trend_multi(list_of_names, label):
     plt.figure(figsize=(15,5))
     for name in list_of_names:
-        plt.plot(range(200), polarity[name], label=name.lower())
+        plt.plot(range(200), polarity[name], label=name)
     plt.title('message polarity trend {}'.format(label))
     plt.legend()
+    plt.ylabel('polarity [-1,1]')
     plt.tight_layout()
     plt.tick_params(axis='x',labelbottom=False)
     plt.savefig('polarity_trend_{}.png'.format(label), dpi=200, bbox_inches='tight')
     plt.cla()
     plt.clf()
     plt.close()
+
+# based on overall metric ranking, plot the polarity of the top 3 and bottom 3 
+def plot_best_worst(k):
+    friend_ranking = sorted(metrics['overall'], key=metrics['overall'].get, reverse=True)
+    best = friend_ranking[:k]
+    worst = friend_ranking[-k:]
+    plot_polarity_trend_multi(best,'best')
+    plot_polarity_trend_multi(worst,'worst')
 
 # get a freq dictionary of words
 def get_words_freq(messages, k):
@@ -175,18 +277,18 @@ def get_words_freq(messages, k):
             continue
         filtered_words = [word.lower() for word in msg['content'].split(' ') if word.lower() not in stopwords]
         for word in filtered_words:
+            word = filter_characters(word)
             if len(word) < 2:
-                continue
-            if not filter_s(word):
                 continue
             if word not in word_freq:
                 word_freq[word] = 0
             word_freq[word] += 1
     return word_freq
 
-# check if word contains non-ascii characters
-def filter_s(word):
-    return all(ord(c) < 128 for c in word)
+# preprocess string
+def filter_characters(s):
+    s = filter(lambda c: ord(c) < 128, s).replace('\\','')
+    return s.decode('unicode-escape').encode('latin1')
 
 # get avg frequence over all people
 def get_average_freq_by_word(row):
@@ -228,7 +330,6 @@ def top_words_heatmap(k):
     random.seed(7)
     random.shuffle(cols)
     df = df[cols]
-    df.columns = [x.lower() for x in df.columns]
     df = df.set_index('word')
     heatmap(df,k)
 
@@ -246,32 +347,48 @@ def heatmap(df,k):
 # calculate metrics
 def analyze(dataset):
     global metrics
+    print 'analzing data'
     for name in dataset:
+        print 'analyzing', name
         metrics['avg_msg'][name] = get_avg_msg(dataset[name])
         metrics['sent_receive_ratio'][name] = get_sent_receive_ratio(dataset[name])
         metrics['sent_receive_word_ratio'][name] = get_sent_receive_word_ratio(dataset[name])
         metrics['responsiveness'][name] = get_responsiveness(dataset[name])
         metrics['avg_polarity'][name] = get_avg_polarity(dataset[name], name)
+        metrics['avg_photo_share'][name] = get_avg_photos(dataset[name], name)
+
+    plot_metric('avg_polarity', 'Avg polarity of messages', 'polarity [-1,1]')
+    plot_metric('avg_msg', 'Avg number of messages per day during friendship', 'messages')
+    plot_metric('avg_photo_share', 'Avg number of photos shared per day during friendship', 'photos')
+    plot_metric('sent_receive_ratio', 'Ratio of messages sent by me to messages received\n', 'ratio')
+    plot_metric('sent_receive_word_ratio', 'Ratio of non-stopwords sent by me to non-stopwords received\n', 'ratio')
+
     for name in metrics['avg_msg']:
         metrics['overall'][name] = get_overall_friendship_score(name)
-    fix_scale('responsiveness')
+
     fix_scale('overall')
+    fix_scale('responsiveness')
+    plot_metric('responsiveness', 'Responsiveness (higher number means quick response)', 'responsiveness')
+    plot_metric('overall', 'Overall friendship score', 'how much you mean to me according\n to math')
+    plot_best_worst(3)
 
 # change the scale to max out at 1
 def fix_scale(metric_name):
     global metrics
     max_value = max(metrics[metric_name].values())
     for name in metrics[metric_name]:
-        metrics[metric_name][name] = round(metrics[metric_name][name]/max_value,2)
+        metrics[metric_name][name] = round(metrics[metric_name][name]/max_value,3)
 
 # store metrics to csv
 def store_metric_results_csv():
+    print 'storing metrics to csv'
     with open('metrics_summary.csv','w') as outfile:
-        outfile.write('name,avg_msg,sent_receive_ratio,sent_receive_word_ratio,responsiveness,avg_polarity,overall_score\n')
+        outfile.write('name,avg_msg,avg_photo_share,sent_receive_ratio,sent_receive_word_ratio,responsiveness,avg_polarity,overall_score\n')
         for name in dataset:
-            outfile.write('{},{},{},{},{},{},{}\n'.format(
+            outfile.write('{},{},{},{},{},{},{},{}\n'.format(
                 name,
                 metrics['avg_msg'][name],
+                metrics['avg_photo_share'][name],
                 metrics['sent_receive_ratio'][name],
                 metrics['sent_receive_word_ratio'][name],
                 metrics['responsiveness'][name],
@@ -287,7 +404,7 @@ def plot_metric(metric_name, title, y_axis):
         sorted_values.append(metric_data[name])
     y_pos = range(len(sorted_names))
     plt.bar(y_pos, sorted_values, align='center', width=0.5)
-    plt.xticks(y_pos, map(lambda x: x.lower(),sorted_names))
+    plt.xticks(y_pos, sorted_names)
     plt.title(title)
     plt.ylabel(y_axis)
     plt.xticks(rotation=90)
@@ -298,29 +415,11 @@ def plot_metric(metric_name, title, y_axis):
     plt.close()
 
 def main():
-    print 'loading dataset'
     load_dict()
-    #pickel_dataset()
-    print 'analyzing dataset'
     analyze(dataset)
-
-    print 'saving metric results'
     store_metric_results_csv()
-
-    friend_ranking = sorted(metrics['overall'], key=metrics['overall'].get, reverse=True)
-    best_5 = friend_ranking[:5]
-    worst_5 = friend_ranking[-5:]
-    plot_polarity_trend_multi(best_5,'best')
-    plot_polarity_trend_multi(worst_5,'worst')
-    
+    save_global_polarity_df()
     top_words_heatmap(10)
 
-    plot_metric('avg_polarity', 'Avg polarity of messages. 1 being positive, -1 being negative', 'polarity')
-    plot_metric('avg_msg', 'Avg number of messages per day during friendship', 'messages')
-    plot_metric('sent_receive_ratio', 'Ratio of messages sent by me to messages received\n (high number is bad)', 'ratio')
-    plot_metric('responsiveness', 'Responsiveness (higher number means quicker to respond)', 'responsiveness')
-    plot_metric('overall', 'Overall friendship score', 'how much you mean to me according\n to this program')
-    plot_metric('sent_receive_word_ratio', 'Ratio of non-stopwords sent by me to non-stopwords received\n (high number is bad)', 'ratio')
-    
 if __name__ == "__main__":
     main()
